178ebc3d28205768cfbe200a110a391514078ac0
darmbrust <daniel.armbrust.list@gmail.com>
2012-12-11 15:34:48 +0000

    More work / refactoring after CSIRO feedback.

--- src/main/java/gov/va/LegoClassifier.java
+++ src/main/java/gov/va/LegoClassifier.java
@@ -9,6 +9,7 @@ import gov.va.legoSchema.LegoList;
 import gov.va.legoSchema.Measurement;
 import gov.va.legoSchema.Point;
 import gov.va.legoSchema.Rel;
+import gov.va.legoSchema.Timing;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -35,7 +36,8 @@ public class LegoClassifier
 
     IReasoner<String> reasoner = new SnorocketReasoner<>();
     Factory<String> f = new Factory<>();
-    Set<String> assertionIds = new HashSet<>();
+    Set<String> conceptIds = new HashSet<>();
+    Set<IAxiom> axioms = new HashSet<>();
 
     public LegoClassifier()
     {
@@ -62,33 +64,37 @@ public class LegoClassifier
             }
         }
         
+        reasoner.classify(axioms);
+        
         // The taxonomy contains the inferred hierarchy
         IOntology<String> t = reasoner.getClassifiedOntology();
 
-        for (String s : assertionIds)
+        for (String s : conceptIds)
         {
             // We can look for nodes using the concept ids.
             Node<String> newNode = t.getNode(s);
             if (newNode == null)
             {
-                //some still fail at the moment
-                continue;
+                System.out.println("No Node " + s);
             }
-            System.out.println("Equivalent Concepts " + newNode.getEquivalentConcepts());
-
-            // We can now look for the parent and child nodes
-            Set<Node<String>> parentNodes = newNode.getParents();
-            System.out.println("Parents:");
-            for (Node<String> parentNode : parentNodes)
+            else
             {
-                System.out.println("  " + parentNode.getEquivalentConcepts());
-            }
-
-            Set<Node<String>> childNodes = newNode.getChildren();
-            System.out.println("Children:");
-            for (Node<String> childNode : childNodes)
-            {
-                System.out.println("  " + childNode.getEquivalentConcepts());
+                System.out.println("Equivalent Concepts " + newNode.getEquivalentConcepts());
+    
+                // We can now look for the parent and child nodes
+                Set<Node<String>> parentNodes = newNode.getParents();
+                System.out.println("Parents:");
+                for (Node<String> parentNode : parentNodes)
+                {
+                    System.out.println("  " + parentNode.getEquivalentConcepts());
+                }
+    
+                Set<Node<String>> childNodes = newNode.getChildren();
+                System.out.println("Children:");
+                for (Node<String> childNode : childNodes)
+                {
+                    System.out.println("  " + childNode.getEquivalentConcepts());
+                }
             }
             
             System.out.println("=====================================");
@@ -97,46 +103,64 @@ public class LegoClassifier
 
     private void classify(Lego l)
     {
-        Set<IAxiom> axioms = new HashSet<>();
-
         for (Assertion a : l.getAssertion())
         {
             // TODO assertion components
-            // TODO timings
-            // TODO IConcept per assertion, or one IConcept per lego?
-            // TODO how often to call classify? Once per lego?
-
-            String id = generateUUID(a);
-            assertionIds.add(id);
-            IConcept assertionConcept = f.createConcept(id);
-
+            // TODO do I have to do the equivalence thing whenever I make a conjunction? 
+            
             // Discernibile
-            process(axioms, a.getDiscernible().getConcept(), assertionConcept);
+            if (a.getDiscernible().getConcept() != null)
+            {
+                process("D", a.getDiscernible().getConcept());
+            }
+            else if (false)
+            {
+                //TODO conjunction
+            }
 
             // Qualifier
-            process(axioms, a.getQualifier().getConcept(), assertionConcept);
+            if (a.getQualifier().getConcept() != null)
+            {
+                process("Q", a.getQualifier().getConcept());
+            }
+            else if (false)
+            {
+                //TODO conjunction
+            }
 
             // Value
             if (a.getValue().getConcept() != null)
             {
-                process(axioms, a.getValue().getConcept(), assertionConcept);
+                process("V", a.getValue().getConcept());
             }
-            else
+            else if (false)
             {
-                logger.error("value measurements not supported yet");
-                continue;
+                //TODO conjunction
+            }
+            else if (a.getValue().getMeasurement() != null)
+            {
+                processMeasurement("Value", a.getValue().getMeasurement());
+            }
+            
+            //Timing
+            if (a.getTiming() != null)
+            {
+                Timing t = a.getTiming();
+                Measurement m = t.getMeasurement();
+                if (m != null)
+                {
+                    processMeasurement("Timing", m);
+                }
             }
         }
-        reasoner.classify(axioms);
     }
 
-    private void process(Set<IAxiom> axioms, Concept dqvFocusConcept, IConcept assertionConcept)
+    private void process(String type, Concept dqvFocusConcept)
     {
-        IConcept focusConcept = f.createConcept(getValuesInConcept(dqvFocusConcept));
-        
+        List<IConcept> rhs = new ArrayList<IConcept>();
+        IConcept focusConcept = f.createConcept(getIdForConcept(dqvFocusConcept)); 
         if (dqvFocusConcept.getRel() != null)
         {
-            List<IConcept> rhs = new ArrayList<IConcept>();
             for (Rel r : dqvFocusConcept.getRel())
             {
                 IConcept temp = createExistential(r);
@@ -148,22 +172,81 @@ public class LegoClassifier
             if (rhs.size() > 0)
             {
                 rhs.add(0, focusConcept);
-                IConcept conjunction = f.createConjunction(rhs.toArray(new IConcept[0]));
-                axioms.add(f.createConceptInclusion(assertionConcept, conjunction));
-                axioms.add(f.createConceptInclusion(conjunction, assertionConcept));
+                IConcept conjunction = f.createConjunction(rhs.toArray(new IConcept[rhs.size()]));
+                
+                //TODO not sure if this ID is being generated correctly
+                IConcept equivConcept = f.createConcept(generateUUID(type, dqvFocusConcept));
+                
+                axioms.add(f.createConceptInclusion(equivConcept, conjunction));
+                axioms.add(f.createConceptInclusion(conjunction, equivConcept));
+            }
+        }
+    }
+    
+    private IConcept processMeasurement(String type, Measurement m)
+    {
+        IConcept unitsConcept = null;
+        if (m.getUnits() != null && m.getUnits().getConcept() != null)
+        {
+            //units are optional
+            unitsConcept = f.createConcept(getIdForConcept(m.getUnits().getConcept()));
+        }
+        
+        if (m.getPoint() != null)
+        {
+            //TODO no idea if this measurement processing is right
+            Point p = m.getPoint();
+            if (p.getNumericValue() != null)
+            {
+                boolean inclusive;
+                if (p.isInclusive() == null)
+                {
+                    inclusive = true;
+                }
+                else
+                {
+                    inclusive = p.isInclusive().booleanValue();
+                }
+                
+                ILiteral literal = f.createDoubleLiteral(p.getNumericValue());
+                
+                INamedFeature<String> feature = f.createFeature("equals:" + p.getNumericValue());
+                IConcept data = f.createDatatype(feature, Operator.EQUALS, literal);
+                
+                if (unitsConcept != null)
+                {
+                    IConcept conjunction = f.createConjunction(unitsConcept, data);
+                    IConcept equivConcept = f.createConcept(type + ":" + getIdForConcept(m.getUnits().getConcept()) + ":equals:" + p.getNumericValue());
+                    axioms.add(f.createConceptInclusion(equivConcept, conjunction));
+                    axioms.add(f.createConceptInclusion(conjunction, equivConcept));
+                    return conjunction;
+                }
+                else
+                {
+                    return data;
+                }
+            }
+            else if (p.getStringValue() != null)
+            {
+                //TODO string constant
+                throw new RuntimeException("string value not yet handled in measurement");
             }
             else
             {
-                axioms.add(f.createConceptInclusion(assertionConcept, focusConcept));
-                axioms.add(f.createConceptInclusion(focusConcept, assertionConcept));
+                throw new RuntimeException("invalid measurement");
             }
+        }
+        else if (m.getInterval() != null)
+        {
+            //TODO interval
+            throw new RuntimeException("measurement interval not yet handeled");
         }
         else
         {
-            axioms.add(f.createConceptInclusion(assertionConcept, focusConcept));
-            axioms.add(f.createConceptInclusion(focusConcept, assertionConcept));
+            throw new RuntimeException("invalid measurement");
         }
     }
+    
 
     private IConcept createExistential(Rel r)
     {
@@ -171,7 +254,7 @@ public class LegoClassifier
         INamedRole<String> typeConcept = f.createRole(getValuesInConcept(r.getType().getConcept()));
         if (r.getDestination().getConcept() != null)
         {
-            IConcept destConcept = f.createConcept(getValuesInConcept(r.getDestination().getConcept()));
+            IConcept destConcept = f.createConcept(getIdForConcept(r.getDestination().getConcept()));
             
             ArrayList<IConcept> nestedExistentials = new ArrayList<IConcept>();
             if (r.getDestination().getConcept().getRel() != null)
@@ -194,66 +277,26 @@ public class LegoClassifier
         }
         else if (r.getDestination().getMeasurement() != null)
         {
-            Measurement m = r.getDestination().getMeasurement();
-            IConcept unitsConcept = null;
-            if (m.getUnits() != null && m.getUnits().getConcept() != null)
-            {
-                //units are optional
-                unitsConcept = f.createConcept(getValuesInConcept(m.getUnits().getConcept()));
-            }
-            
-            if (m.getPoint() != null)
-            {
-                Point p = m.getPoint();
-                if (p.getNumericValue() != null)
-                {
-                    ILiteral literal = f.createDoubleLiteral(p.getNumericValue());
-                    
-                    INamedFeature<String> feature = f.createFeature("equals:" + p.getNumericValue());
-                    IConcept data = f.createDatatype(feature, Operator.EQUALS, literal);
-                    
-                    return f.createExistential(typeConcept, (unitsConcept == null ? data : f.createConjunction(unitsConcept, data)));
-                }
-                else if (p.getStringValue() != null)
-                {
-                    throw new RuntimeException("string value not yet handled in measurement");
-                }
-            }
-            else if (m.getInterval() != null)
-            {
-                throw new RuntimeException("measurement interval not yet handeled");
-            }
-            else
-            {
-                throw new RuntimeException("invalid measurement");
-            }
+            IConcept measurementConcept = processMeasurement("Destination", r.getDestination().getMeasurement());
+            return f.createExistential(typeConcept, measurementConcept);
         }
         else
         {
             throw new RuntimeException("invalid rel");
         }
-        throw new RuntimeException("oops");
     }
 
     /**
      * Generate a unique ID based off of the concept (and all subconcepts)
      */
-    private String generateUUID(Assertion a)
+    private String generateUUID(String type, Concept c)
     {
         StringBuilder sb = new StringBuilder();
-        buildIDComponents(sb, a.getDiscernible().getConcept());
-        buildIDComponents(sb, a.getQualifier().getConcept());
-        if (a.getValue().getConcept() != null)
-        {
-            buildIDComponents(sb, a.getValue().getConcept());
-        }
-        else
-        {
-            sb.append(getValuesInInterval(a.getValue().getMeasurement().getInterval()));
-            sb.append(getValueInPoint(a.getValue().getMeasurement().getPoint()));
-        }
+        sb.append(type);
+        buildIDComponents(sb, c);
         UUID uuid = UUID.nameUUIDFromBytes(sb.toString().getBytes());
         logger.debug("Created " + uuid.toString() + " from " + sb.toString());
+        conceptIds.add(uuid.toString());
         return uuid.toString();
     }
 
@@ -282,17 +325,26 @@ public class LegoClassifier
             }
         }
     }
+    
+    private String getIdForConcept(Concept c)
+    {
+        String s = getValuesInConcept(c);
+        conceptIds.add(s);
+        return s;
+    }
 
     private String getValuesInConcept(Concept c)
     {
+        String conceptId = null;
         if (c.getSctid() != null)
         {
-            return c.getSctid().toString();
+            conceptId = c.getSctid().toString(); 
         }
         else
         {
-            return c.getUuid();
+            conceptId = c.getUuid();
         }
+        return conceptId;
     }
 
     private String getValuesInInterval(Interval i)
